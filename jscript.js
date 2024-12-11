1746d4e0dabb8456b496a1060be2ffdfa8388e77
andrea <ramora.drea@gmail.com>
2024-12-11 16:24:14 -0700

    meow2
    meow

--- jscript.js
+++ jscript.js
@@ -1,30 +1,30 @@
-animal.addEventListener("change",function () {
+questions.addEventListener("change",function () {
     console.log("change");  //change -> everytime you pick different options
-    let choice = document.getElementById("animal").value;
+    let choice = document.getElementById("questions").value;
     console.log(choice);
     
-    if (choice=="bunny"){ //two "=" means evaluation
-        console.log("it is bunny!");
-        document.getElementById('animalmessage').innerHTML = 
-        "B-Hop ૮꒰ ˶• ༝ •˶꒱ა ♡";
+    if (choice=="answer1"){ //two "=" means evaluation
+        console.log("qethnicity");
+        document.getElementById('answerinput').innerHTML = 
+        "Raffy is a mix of Hispanic and Indigenous! ૮꒰ ˶• ༝ •˶꒱ა ♡";
     }
 
-    if (choice=="cat") {
-        console.log("meowmeow");
-        document.getElementById('animalmessage').innerHTML = 
-        "Meow Meow ᓚᘏᗢ";
+    if (choice=="answer2") {
+        console.log("qhobby");
+        document.getElementById('answerinput').innerHTML = 
+        "Raffy Likes building legos and playing games; He especially loves building lego cars. ᓚᘏᗢ";
     }
 
-    if (choice=="dog"){
-        console.log("woofwoof");
-        document.getElementById('animalmessage').innerHTML = 
-        "Doggo •ﻌ•";
+    if (choice=="answer3"){
+        console.log("qsanrio");
+        document.getElementById('answerinput').innerHTML = 
+        "Raffy's favorite Sanrio character would be chococat! •ﻌ•";
     }
 
-    if (choice=="capybara") {
-        console.log("capybararara");
-        document.getElementById('animalmessage').innerHTML = 
-        "It's capybara time baby";
+    if (choice=="answer4") {
+        console.log("qpokemon");
+        document.getElementById('answerinput').innerHTML = 
+        "Raffy's favorite pokemon is Oshawatt.";
     }
 })
 
